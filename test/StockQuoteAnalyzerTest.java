5ff3be701b57acd999ec7fabbe14480fae9212e7
flisslj <flisslj@msoe.edu>
2017-04-28 20:15:39 -0500

    Added Tests and updated writeup.md

--- test/StockQuoteAnalyzerTest.java
+++ test/StockQuoteAnalyzerTest.java
@@ -38,6 +38,124 @@ public class StockQuoteAnalyzerTest {
         analyzer = new StockQuoteAnalyzer("ZZZZZZZZZ", generatorMock, audioMock);
     }
 
+    @Test (expectedExceptions = InvalidAnalysisState.class)
+    public void getCurrentPriceShouldThrowExceptionWhenCurrentQuoteIsNull() throws Exception{
+        analyzer = new StockQuoteAnalyzer("A", generatorMock, audioMock);
+
+        analyzer.getCurrentPrice();
+    }
+
+    @Test
+    public void getCurrentPriceShouldReturnCurrentPrice()throws Exception{
+        double previousClose = 12.44;
+        double lastTrade = 15.94;
+        double change = 0.06;
+        StockQuote mockQuote = new StockQuote("A",previousClose,lastTrade,change);
+        when(generatorMock.getCurrentQuote()).thenReturn(mockQuote);
+        analyzer = new StockQuoteAnalyzer("A", generatorMock, audioMock);
+
+        analyzer.refresh();
+        double price = analyzer.getCurrentPrice();
+
+        assertEquals(price, lastTrade);
+    }
+
+    @Test(expectedExceptions = InvalidAnalysisState.class)
+    public void getChangeSinceCloseShouldShouldThrowExceptionWhenTheCurrentQuoteIsNull() throws Exception{
+        analyzer = new StockQuoteAnalyzer("A", generatorMock, audioMock);
+
+        analyzer.getChangeSinceClose();
+    }
+
+    @Test
+    public void getChangeSinceCloseShouldReturnChange() throws Exception{
+        double previousClose = 12.44;
+        double lastTrade = 15.94;
+        double change = 0.06;
+        StockQuote mockQuote = new StockQuote("A",previousClose,lastTrade,change);
+        when(generatorMock.getCurrentQuote()).thenReturn(mockQuote);
+        analyzer = new StockQuoteAnalyzer("A", generatorMock, audioMock);
+
+        analyzer.refresh();
+        double changeSinceClose = analyzer.getChangeSinceClose();
+
+        assertEquals(change,changeSinceClose);
+    }
+
+    @Test
+    public void getPercentChangeSinceCloseShouldReturnPercentChange() throws Exception{
+        double previousClose = 12.44;
+        double lastTrade = 15.94;
+        double change = 0.06;
+        StockQuote mockQuote = new StockQuote("A",previousClose,lastTrade,change);
+        when(generatorMock.getCurrentQuote()).thenReturn(mockQuote);
+        analyzer = new StockQuoteAnalyzer("A", generatorMock, audioMock);
+
+        double percentChange = (change/previousClose) * 100;
+        analyzer.refresh();
+        double percentChangeSinceClose = analyzer.getPercentChangeSinceClose();
+
+
+        assertEquals(percentChange,percentChangeSinceClose);
+    }
+
+
+
+    @Test(expectedExceptions = InvalidAnalysisState.class)
+    public void getPercentChangeSinceCloseShouldShouldThrowExceptionWhenTheCurrentQuoteIsNull() throws Exception{
+        analyzer = new StockQuoteAnalyzer("A", generatorMock, audioMock);
+
+        analyzer.getPercentChangeSinceClose();
+    }
+
+    @Test
+    public void changeSinceLastCheckShouldReturn0ifValueHasNotChanged()throws Exception{
+
+        double previousClose = 12.44;
+        double lastTrade = 15.94;
+        double change = 0.06;
+        StockQuote mockQuote = new StockQuote("A",previousClose,lastTrade,change);
+        when(generatorMock.getCurrentQuote()).thenReturn(mockQuote);
+        analyzer = new StockQuoteAnalyzer("A", generatorMock, audioMock);
+
+        analyzer.refresh();
+        analyzer.refresh();
+        double changeSinceLastCheck = analyzer.getChangeSinceLastCheck();
+
+
+        assertEquals(0.0, changeSinceLastCheck);
+    }
+
+    @Test(expectedExceptions = InvalidAnalysisState.class)
+    public void changeSinceLastCheckShouldThrowExceptionIfPreviousQuoteIsNull()throws Exception {
+
+        analyzer = new StockQuoteAnalyzer("A", generatorMock, audioMock);
+
+        analyzer.refresh(); //this sets the quote the first time but previous quote should still be null
+        analyzer.getChangeSinceLastCheck();
+
+    }
+
+    @Test
+    public void changeSinceLastCheckShouldReturnChangeifValueHasChanged()throws Exception{
+
+        double previousClose = 12.44;
+        double lastTrade = 15.94;
+        double change = 0.06;
+        StockQuote mockQuote = new StockQuote("A",previousClose,lastTrade,change);
+        when(generatorMock.getCurrentQuote()).thenReturn(mockQuote);
+        analyzer = new StockQuoteAnalyzer("A", generatorMock, audioMock);
+
+        analyzer.refresh();
+        double newLastTrade = 16.94;
+        mockQuote = new StockQuote("A",previousClose, lastTrade, change);
+        analyzer.refresh();
+        double changeSinceLastCheck = analyzer.getChangeSinceLastCheck();
+        double actualChange = newLastTrade - lastTrade;
+
+        assertEquals(actualChange, changeSinceLastCheck);
+    }
+
     @Test(expectedExceptions = NullPointerException.class)
     public void constructorShouldThrowExceptionWhenStockQuoteSourceIsNull() throws Exception {
         String validSymbol = "ZUMZ";
--- writeup.md
+++ writeup.md
@@ -10,6 +10,11 @@ Original Line Number | Fault Description | Fix Description
 119 | Happy music played for all positive values instead of only above 1 | ... |
 151 | Throws exception unexpectedly | ... |
 154 | Gets the close of the current quote instead of the previous | ... |
+l85 | getChangeSinceClose throws a null pointer exception instead of a InvalidStateException| Changed the exception thrown to an invalidStateException |
+187 | getChangeSinceClose returns an incorrect value | removed -currentQuote.getClose |
+204 | getPercentChangeSinceClose returns 10X the value its supposed to | changed *100000 to *10000 |
+220 | getChangeSinceLastCheck will always return 0.0 because its subtracting the same value from itself| changed -currentQuote.getLastTrade() to -previousQuote.getLastTrade|
+220 | getChangeSinceLastCheck doesn't throw an invalid state exeption if there is no previous quote | added an invalidAnalysisState that is thrown when previousQuote is null |
 
 
 ## Final code coverage:
